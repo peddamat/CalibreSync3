32c4f47a6b266998290d04f6a8e758b83d8100a7
Sam Peddamatham <peddamat@gmail.com>
2019-12-03 08:41:55 -0500

    Clean-up commit.

--- CalibreSync3/Sharing/FilePresenterView.swift
+++ CalibreSync3/Sharing/FilePresenterView.swift
@@ -9,81 +9,81 @@
 import SwiftUI
 
 struct FilePresenterUIView : UIViewRepresentable {
-
-  var file: URL
-  var showing = false
-  var onDismiss: () -> ()
-
-//  init(file: URL, onDismiss) {
-//    self.file = file
-//  }
-
-  typealias UIViewType = ProvideViewCoordsView
-
-  // Provides a callback that we use to display the controller from the right rect on screen
-  class ProvideViewCoordsView : UIView {
-
-    var didAppear : (UIView, CGRect) -> Void = {_, _ in }
-
-    override func didMoveToWindow() {
-      if self.window == nil { return }
-      didAppear(self, self.bounds)
-    }
-  }
-
-  class Coordinator : NSObject, UIDocumentInteractionControllerDelegate {
-    typealias Parent = FilePresenterUIView
-
-    let parent: Parent
-    let interaction: UIDocumentInteractionController
-
-    init(_  v: Parent) {
-      self.parent = v
-      let url = v.file
-      interaction = UIDocumentInteractionController(url: url)
-      super.init()
-      interaction.delegate = self
-    }
-
-    // TODO: The modal isn't reliably dismissed, fix this
-    func documentInteractionControllerDidDismissOpenInMenu(_ controller: UIDocumentInteractionController) {
-        print("should dismiss")
-      self.parent.onDismiss()
-    }
-
-    func setup(_ v: ProvideViewCoordsView) {
-      v.didAppear = {view, rect in
-        print("Presenter layout \(view) \(rect)")
-        if !self.parent.showing {
-          self.interaction.presentOpenInMenu(from: rect, in: view, animated: true)
+    
+    var file: URL
+    var showing = false
+    var onDismiss: () -> ()
+    
+    //  init(file: URL, onDismiss) {
+    //    self.file = file
+    //  }
+    
+    typealias UIViewType = ProvideViewCoordsView
+    
+    // Provides a callback that we use to display the controller from the right rect on screen
+    class ProvideViewCoordsView : UIView {
+        
+        var didAppear : (UIView, CGRect) -> Void = {_, _ in }
+        
+        override func didMoveToWindow() {
+            if self.window == nil { return }
+            didAppear(self, self.bounds)
         }
-      }
     }
-  }
-
-  typealias _Context = UIViewRepresentableContext<FilePresenterUIView>
-
-
-  // TODO: See if we can completely hide the ugly gray background...
-  func makeUIView(context: _Context) -> ProvideViewCoordsView {
-    let v = ProvideViewCoordsView(frame: CGRect(origin: .zero, size: CGSize(width: 10, height: 10)))
-    v.backgroundColor = UIColor.darkGray.withAlphaComponent(0.5)
-    v.setNeedsLayout()
-    context.coordinator.setup(v)
-    return v
-  }
-
-  func updateUIView(_ uiView: UIViewType, context: _Context) {
-    if let file = context.coordinator.interaction.url {
-      if file != self.file {
-        print("Changed URL unexpectedly!")
-      }
+    
+    class Coordinator : NSObject, UIDocumentInteractionControllerDelegate {
+        typealias Parent = FilePresenterUIView
+        
+        let parent: Parent
+        let interaction: UIDocumentInteractionController
+        
+        init(_  v: Parent) {
+            self.parent = v
+            let url = v.file
+            interaction = UIDocumentInteractionController(url: url)
+            super.init()
+            interaction.delegate = self
+        }
+        
+        // TODO: The modal isn't reliably dismissed, fix this
+        func documentInteractionControllerDidDismissOpenInMenu(_ controller: UIDocumentInteractionController) {
+            print("should dismiss")
+            self.parent.onDismiss()
+        }
+        
+        func setup(_ v: ProvideViewCoordsView) {
+            v.didAppear = {view, rect in
+                print("Presenter layout \(view) \(rect)")
+                if !self.parent.showing {
+                    self.interaction.presentOpenInMenu(from: rect, in: view, animated: true)
+                }
+            }
+        }
     }
-  }
-
-  func makeCoordinator() -> Coordinator {
-    return Coordinator(self)
-  }
-
+    
+    typealias _Context = UIViewRepresentableContext<FilePresenterUIView>
+    
+    
+    // TODO: See if we can completely hide the ugly gray background...
+    func makeUIView(context: _Context) -> ProvideViewCoordsView {
+        let v = ProvideViewCoordsView(frame: CGRect(origin: .zero, size: CGSize(width: 10, height: 10)))
+        v.backgroundColor = UIColor.darkGray.withAlphaComponent(0.5)
+        v.setNeedsLayout()
+        context.coordinator.setup(v)
+        return v
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: _Context) {
+        if let file = context.coordinator.interaction.url {
+            if file != self.file {
+                print("Changed URL unexpectedly!")
+            }
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
 }
 
