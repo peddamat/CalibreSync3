405b0e083e9375a560e27a32618ab5034aa579a2
Sam Peddamatham <peddamat@gmail.com>
2019-12-03 04:10:22 -0500

    Some tantalizing progress. Turns out the calls to pull the calibrePath and dbQueue were very expensive.  By caching those values, we get a significant speed-up.  I tried hardcoding the book cover to a single URL hosted on the SMB file:/// share.  This resulted in an incredibly fast loading time.  When I replaced the line with code to retrieve each individual book cover, the load time decreased significantly.  More specifically, the lag before the initial book cover grid is displayed is increased significantly.

--- CalibreSync3/ContentView.swift
+++ CalibreSync3/ContentView.swift
@@ -10,18 +10,25 @@ import GRDB
 import QGrid
 import SwiftUI
 import Combine
+import KingfisherSwiftUI
 
 // The shared database queue
-var dbQueue: DatabaseQueue!
+var dbQueue: DatabaseQueue?
 
 struct ContentView: View {
     @EnvironmentObject var settingStore: SettingStore
     @State private var showSettings = false
+    var calibrePath: String
 
     func getDBqueue() -> DatabaseQueue {
+        
+        if dbQueue != nil {
+            return dbQueue!
+        }
+        
         let calibreDB = CalibreDB(settingStore: settingStore)
-        let dbQueue = calibreDB.load()
-        return dbQueue
+        dbQueue = calibreDB.load()
+        return dbQueue!
     }
     
     func getBooks() -> [Book] {
@@ -32,7 +39,7 @@ struct ContentView: View {
 
         do {
             try dbQueue.read { db -> [Book] in
-                books = try Book.limit(20).fetchAll(db)
+                books = try Book.limit(100).fetchAll(db)
                 return books!
             }
         } catch {
@@ -51,18 +58,59 @@ struct ContentView: View {
     }
     
     var body: some View {
-        NavigationView {
-            VStack {
-                QGrid(getBooks(), columns: 3) {
-                    GridCell(book: $0, calibreLibraryPath: self.settingStore.getCalibrePath(), dbQueue: self.getDBqueue())
+        VStack {
+            NavigationView {
+                VStack {
+                    QGrid(getBooks(), columns: 3) { book in
+//                        GridCell(book: book, calibreLibraryPath: self.calibrePath, dbQueue: self.getDBqueue())
+
+                        ZStack {
+//                            Image("cover")
+//                                .resizable()
+//                                .aspectRatio(contentMode: .fit)
+//                                .frame(width: 150, height: 150)
+            
+//                            ImageStubView(withURL: "/private/var/mobile/Library/LiveFiles/com.apple.filesystems.smbclientd/Jg110QPublic/Old/Ebook%20Library/Atul%20S.%20Khot/Scala%20Functional%20Programming%20Patter%20(411)/cover.jpg", withPath: self.calibrePath, withDB: self.getDBqueue())
+                            
+//                            ImageView(withURL: "file:///private/var/mobile/Library/LiveFiles/com.apple.filesystems.smbclientd/Jg110QPublic/Old/Ebook Library/Atul S. Khot/Scala Functional Programming Patter (411)/cover.jpg".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+//                            .padding([.horizontal, .top], 2.0)
+
+                            ImageView(withURL: URL(fileURLWithPath: self.calibrePath + "/" + book.path + "/cover.jpg").absoluteString)
+                                .padding([.horizontal, .top], 2.0)
+
+                            Text(book.title)
+                        }
+                    }
+                }
+                .navigationBarTitle("CalibreSync")
+                .navigationBarItems(trailing: profileButton)
+                .sheet(isPresented: $showSettings) {
+                    SettingsView().environmentObject(self.settingStore)
                 }
             }
-            .navigationBarTitle("CalibreSync")
-            .navigationBarItems(trailing: profileButton)
-            .sheet(isPresented: $showSettings) {
-                SettingsView().environmentObject(self.settingStore)
-            }
         }
+    }
+}
+
+struct ImageStubView: View {
+//    @State var image:UIImage = UIImage()
+    
+    init(withURL url:String, withPath: String, withDB: DatabaseQueue) {
+        print(url)
+        let h = withDB
+    }
+    
+    var body: some View {
+//        VStack {
+            Image("cover")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:150, height:150)
+            .padding(10)
+//        }
+//        }.onReceive(imageLoader.didChange) { data in
+//            self.image = UIImage(data: data) ?? UIImage()
+//        }
     }
 }
 
@@ -75,14 +123,21 @@ class ImageLoader: ObservableObject {
     }
     
     init(urlString:String) {
-        guard let url = URL(string: urlString) else { return }
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else { return }
-            DispatchQueue.main.async {
-                self.data = data
-            }
+        
+        guard let url = URL(string: urlString) else {
+            print("Punt! \(urlString)")
+            return
         }
-        task.resume()
+        
+//        DispatchQueue.global(qos: .userInitiated).async {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let data = data else { return }
+                DispatchQueue.main.async {
+                    self.data = data
+                }
+            }
+            task.resume()
+//        }
     }
 }
 
@@ -92,7 +147,7 @@ struct ImageView: View {
     
     init(withURL url:String) {
         imageLoader = ImageLoader(urlString:url)
-        print(url)
+//        print("Fuck: \(url)")
     }
     
     var body: some View {
@@ -108,6 +163,8 @@ struct ImageView: View {
     }
 }
 
+
+
 struct GridCell: View {
     var book: Book
     var calibreLibraryPath: String
@@ -115,25 +172,43 @@ struct GridCell: View {
 
     var body: some View {
         VStack() {
-            NavigationLink(destination: BooksDetail(book: book, calibrePath: calibreLibraryPath, dbQueue: dbQueue)) {
+//            NavigationLink(destination: BooksDetail(book: book, calibrePath: calibreLibraryPath, dbQueue: dbQueue)) {
 //                Image("cover")
 //                    .resizable()
 //                    .aspectRatio(contentMode: .fit)
 //                    .frame(width: 150, height: 150)
                 
-                ImageView(withURL: URL(fileURLWithPath: calibreLibraryPath + "/" + book.path + "/cover.jpg").absoluteString)
-                    .padding([.horizontal, .top], 2.0)
+            ImageStubView(withURL: URL(fileURLWithPath: calibreLibraryPath + "/" + book.path + "/cover.jpg").absoluteString, withPath: calibreLibraryPath, withDB: dbQueue)
+            
+//                ImageView(withURL: URL(fileURLWithPath: calibreLibraryPath + "/" + book.path + "/cover.jpg").absoluteString)
+//                    .padding([.horizontal, .top], 2.0)
+                
+//                KFImage(URL(fileURLWithPath: calibreLibraryPath + "/" + book.path + "/cover.jpg"))
+//                .onSuccess { r in
+//                    print("suc: \(r)")
+//                }
+//                .onFailure { e in
+//                    print("err: \(e)")
+//                }
+//                .placeholder {
+//                    Image(systemName: "arrow.2.circlepath.circle")
+//                        .font(.largeTitle)
+//                }
+//                .resizable()
+//                .frame(width: 150, height: 150)
+//                .cornerRadius(20)
+//                .shadow(radius: 5)
                 
     //            Text(book.title)
-            }
-            .buttonStyle(PlainButtonStyle())
+//            }
+//            .buttonStyle(PlainButtonStyle())
         }
     }
 }
     
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environmentObject(SettingStore())
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView().environmentObject(SettingStore())
+//    }
+//}
