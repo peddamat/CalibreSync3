778d9247742e5a54e06dd46f5b7a2502c296ac40
Sam Peddamatham <peddamat@gmail.com>
2019-12-03 04:22:14 -0500

    Initial clean-up complete.

--- CalibreSync3/ContentView.swift
+++ CalibreSync3/ContentView.swift
@@ -82,50 +82,34 @@ struct NewGridCell: View {
     
     var body: some View {
         NavigationLink(destination: BooksDetail(book: book, calibrePath: calibrePath, dbQueue: dbQueue)) {
-            
-//                        GridCell(book: book, calibreLibraryPath: self.calibrePath, dbQueue: self.getDBqueue())
-            
             ZStack {
-//                            Image("cover")
-//                                .resizable()
-//                                .aspectRatio(contentMode: .fit)
-//                                .frame(width: 150, height: 150)
-
-//                            ImageStubView(withURL: "/private/var/mobile/Library/LiveFiles/com.apple.filesystems.smbclientd/Jg110QPublic/Old/Ebook%20Library/Atul%20S.%20Khot/Scala%20Functional%20Programming%20Patter%20(411)/cover.jpg", withPath: self.calibrePath, withDB: self.getDBqueue())
-
-//                            ImageView(withURL: "file:///private/var/mobile/Library/LiveFiles/com.apple.filesystems.smbclientd/Jg110QPublic/Old/Ebook Library/Atul S. Khot/Scala Functional Programming Patter (411)/cover.jpg".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
-//                            .padding([.horizontal, .top], 2.0)
-                
                 ImageView(withURL: URL(fileURLWithPath: self.calibrePath + "/" + book.path + "/cover.jpg").absoluteString)
                     .padding([.horizontal, .top], 2.0)
-                
 //                Text(book.title)
             }
         }
         .buttonStyle(PlainButtonStyle())
-
     }
 }
 
-struct ImageStubView: View {
-//    @State var image:UIImage = UIImage()
+struct ImageView: View {
+    @ObservedObject var imageLoader:ImageLoader
+    @State var image:UIImage = UIImage()
     
-    init(withURL url:String, withPath: String, withDB: DatabaseQueue) {
-        print(url)
-        let h = withDB
+    init(withURL url:String) {
+        imageLoader = ImageLoader(urlString:url)
     }
     
     var body: some View {
-//        VStack {
-            Image("cover")
+        VStack {
+            Image(uiImage: image)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width:150, height:150)
             .padding(10)
-//        }
-//        }.onReceive(imageLoader.didChange) { data in
-//            self.image = UIImage(data: data) ?? UIImage()
-//        }
+        }.onReceive(imageLoader.didChange) { data in
+            self.image = UIImage(data: data) ?? UIImage()
+        }
     }
 }
 
@@ -155,72 +139,6 @@ class ImageLoader: ObservableObject {
 //        }
     }
 }
-
-struct ImageView: View {
-    @ObservedObject var imageLoader:ImageLoader
-    @State var image:UIImage = UIImage()
-    
-    init(withURL url:String) {
-        imageLoader = ImageLoader(urlString:url)
-//        print("Fuck: \(url)")
-    }
-    
-    var body: some View {
-        VStack {
-            Image(uiImage: image)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width:150, height:150)
-            .padding(10)
-        }.onReceive(imageLoader.didChange) { data in
-            self.image = UIImage(data: data) ?? UIImage()
-        }
-    }
-}
-
-
-
-struct GridCell: View {
-    var book: Book
-    var calibreLibraryPath: String
-    var dbQueue: DatabaseQueue
-
-    var body: some View {
-        VStack() {
-//            NavigationLink(destination: BooksDetail(book: book, calibrePath: calibreLibraryPath, dbQueue: dbQueue)) {
-//                Image("cover")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .frame(width: 150, height: 150)
-                
-            ImageStubView(withURL: URL(fileURLWithPath: calibreLibraryPath + "/" + book.path + "/cover.jpg").absoluteString, withPath: calibreLibraryPath, withDB: dbQueue)
-            
-//                ImageView(withURL: URL(fileURLWithPath: calibreLibraryPath + "/" + book.path + "/cover.jpg").absoluteString)
-//                    .padding([.horizontal, .top], 2.0)
-                
-//                KFImage(URL(fileURLWithPath: calibreLibraryPath + "/" + book.path + "/cover.jpg"))
-//                .onSuccess { r in
-//                    print("suc: \(r)")
-//                }
-//                .onFailure { e in
-//                    print("err: \(e)")
-//                }
-//                .placeholder {
-//                    Image(systemName: "arrow.2.circlepath.circle")
-//                        .font(.largeTitle)
-//                }
-//                .resizable()
-//                .frame(width: 150, height: 150)
-//                .cornerRadius(20)
-//                .shadow(radius: 5)
-                
-    //            Text(book.title)
-//            }
-//            .buttonStyle(PlainButtonStyle())
-        }
-    }
-}
-    
 
 //struct ContentView_Previews: PreviewProvider {
 //    static var previews: some View {
